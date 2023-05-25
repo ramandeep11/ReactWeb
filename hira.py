8f7ddb53764c78844541b6be59ba89e90d8bd0dd
ramandeep <dilumaan70@gmail.com>
2023-05-25 15:51:25 +0530

    1st commit

---
+++ hira.py
@@ -0,0 +1,137 @@
+from random import randrange
+from collections import Counter
+import time
+
+hiragana = [
+    [["a", "あ"], ["i", "い"], ["u", "う"], ["e", "え"], ["o", "お"]],
+[["ka", "か"], ["ki", "き"], ["ku", "く"], ["ke", "け"], ["ko", "こ"]],
+[["sa", "さ"], ["shi", "し"], ["su", "す"], ["se", "せ"], ["so", "そ"]],
+[["ta", "た"], ["chi", "ち"], ["tsu", "つ"], ["te", "て"], ["to", "と"]],
+[["na", "な"], ["ni", "に"], ["nu", "ぬ"], ["ne", "ね"], ["no", "の"]],
+[["ha", "は"], ["hi", "ひ"], ["fu", "ふ"], ["he", "へ"], ["ho", "ほ"]],
+[["ma", "ま"], ["mi", "み"], ["mu", "む"], ["me", "め"], ["mo", "も"]],
+[["ya", "や"], ["yu", "ゆ"], ["yo", "よ"]],
+[["ra", "ら"], ["ri", "り"], ["ru", "る"], ["re", "れ"], ["ro", "ろ"]],
+[["wa", "わ"], ["wo", "を"]],
+[["n", "ん"]],
+[["ga", "が"], ["gi", "ぎ"], ["gu", "ぐ"], ["ge", "げ"], ["go", "ご"]],
+[["za", "ざ"], ["ji", "じ"], ["zu", "ず"], ["ze", "ぜ"], ["zo", "ぞ"]],
+[["da", "だ"], ["ji", "ぢ"], ["zu", "づ"], ["de", "で"], ["do", "ど"]],
+[["ba", "ば"], ["bi", "び"], ["bu", "ぶ"], ["be", "べ"], ["bo", "ぼ"]],
+[["pa", "ぱ"], ["pi", "ぴ"], ["pu", "ぷ"], ["pe", "ぺ"], ["po", "ぽ"]],
+[["kya", "きゃ"], ["kyu", "きゅ"], ["kyo", "きょ"]],
+[["sha", "しゃ"], ["shu", "しゅ"], ["sho", "しょ"]],
+[["cha", "ちゃ"], ["chu", "ちゅ"], ["cho", "ちょ"]],
+[["nya", "にゃ"], ["nyu", "にゅ"], ["nyo", "にょ"]],
+[["hya", "ひゃ"], ["hyu", "ひゅ"], ["hyo", "ひょ"]],
+[["mya", "みゃ"], ["myu", "みゅ"], ["myo", "みょ"]],
+[["rya", "りゃ"], ["ryu", "りゅ"], ["ryo", "りょ"]]
+
+]
+
+katakana = [
+    [["a", "ア"], ["i", "イ"], ["u", "ウ"], ["e", "エ"], ["o", "オ"]],
+[["ka", "カ"], ["ki", "キ"], ["ku", "ク"], ["ke", "ケ"], ["ko", "コ"]],
+[["sa", "サ"], ["shi", "シ"], ["su", "ス"], ["se", "セ"], ["so", "ソ"]],
+[["ta", "タ"], ["chi", "チ"], ["tsu", "ツ"], ["te", "テ"], ["to", "ト"]],
+[["na", "ナ"], ["ni", "ニ"], ["nu", "ヌ"], ["ne", "ネ"], ["no", "ノ"]],
+[["ha", "ハ"], ["hi", "ヒ"], ["fu", "フ"], ["he", "ヘ"], ["ho", "ホ"]],
+[["ma", "マ"], ["mi", "ミ"], ["mu", "ム"], ["me", "メ"], ["mo", "モ"]],
+[["ya", "ヤ"], ["yu", "ユ"], ["yo", "ヨ"]],
+[["ra", "ラ"], ["ri", "リ"], ["ru", "ル"], ["re", "レ"], ["ro", "ロ"]],
+[["wa", "ワ"], ["wo", "ヲ"]],
+[["n", "ン"]],
+[["ga", "ガ"], ["gi", "ギ"], ["gu", "グ"], ["ge", "ゲ"], ["go", "ゴ"]],
+[["za", "ザ"], ["ji", "ジ"], ["zu", "ズ"], ["ze", "ゼ"], ["zo", "ゾ"]],
+[["da", "ダ"], ["ji", "ヂ"], ["zu", "ヅ"], ["de", "デ"], ["do", "ド"]],
+[["ba", "バ"], ["bi", "ビ"], ["bu", "ブ"], ["be", "ベ"], ["bo", "ボ"]],
+[["pa", "パ"], ["pi", "ピ"], ["pu", "プ"], ["pe", "ペ"], ["po", "ポ"]],
+[["kya", "キャ"], ["kyu", "キュ"], ["kyo", "キョ"]],
+[["sha", "シャ"], ["shu", "シュ"], ["sho", "ショ"]],
+[["cha", "チャ"], ["chu", "チュ"], ["cho", "チョ"]],
+[["nya", "ニャ"], ["nyu", "ニュ"], ["nyo", "ニョ"]],
+[["hya", "ヒャ"], ["hyu", "ヒュ"], ["hyo", "ヒョ"]],
+[["mya", "ミャ"], ["myu", "ミュ"], ["myo", "ミョ"]],
+[["rya", "リャ"], ["ryu", "リュ"], ["ryo", "リョ"]]
+
+]
+
+testtype = [
+    [["hiragana", "hira", "hi", "h"], hiragana],
+    [["katakana", "kata", "ka", "k"], katakana]
+]
+
+print("  Welcome to Hiragana Trivia! ")
+print("  Hiragana / Katakana / Both  ")
+typeinput = input().lower()
+# print("a/ka/sa/ta/na/ha/ma/ya/ra/wa/n")
+# startfrom = input("Choose from: ").lower()
+# stopat    = input("to: ").lower()
+
+startindex = -1
+stopindex = -1
+# for i in range(len(hiragana)):
+#     if(startfrom==hiragana[i][0][0]):
+#         startindex = i
+#     if(stopat==hiragana[i][0][0]):
+#         stopindex = i
+startindex=0
+stopindex=22
+if( startindex == -1 or stopindex == -1 ):
+    print("Can't seem to find list.")
+    exit()
+
+questionlist = []
+for el in testtype:
+    if ( typeinput == "both" or typeinput == "b" ):
+        for i in range(startindex,stopindex+1):
+            for b in hiragana[i]:
+                questionlist.append(b)
+            for c in katakana[i]:
+                questionlist.append(c)
+        break
+    else:
+        for a in el[0]:
+            if ( a == typeinput ):
+                for i in range(startindex,stopindex+1):
+                    for b in el[1][i]:
+                        questionlist.append(b)
+                break
+
+if(len(questionlist) == 0):
+    print("Can't seem to find list.")
+    exit()
+
+wrongcount = 0
+wronglist = []
+wronglistromanji = []
+testlist = questionlist
+starttime = time.time()
+while True:
+    if(len(testlist)==0):
+        endtime = time.time()
+        break
+    curquestindex = randrange(len(testlist))
+    print("What is this :" + testlist[curquestindex][1] )
+    answ = input().lower()
+    if( answ == testlist[curquestindex][0] ):
+        testlist.remove(testlist[curquestindex])
+        print("Correct!!!")
+    else:
+        print("Wrong, please try again")
+        wronglist.append(testlist[curquestindex][1])
+        wronglistromanji.append(testlist[curquestindex][0])
+        wrongcount += 1
+
+print("Time elapsed : " + str(round(endtime-starttime, 2)) + " seconds")
+print("You have finish from {} to {} with {} mistakes".format(startfrom.upper(),stopat.upper(),wrongcount))
+if(wrongcount!=0):
+    print("Most mistake is :", end='')
+    result = [item for items, c in Counter(wronglist).most_common() 
+        for item in [items] * c]
+    resultromanji = [item for items, c in Counter(wronglistromanji).most_common() 
+        for item in [items] * c]
+    result = list(dict.fromkeys(result))[:5]
+    resultromanji = list(dict.fromkeys(resultromanji))[:5]
+    for i in range(len(result)):
+        print(" " + result[i] + "(" + resultromanji[i] + ")", end="")
--- src/App.js
+++ src/App.js
@@ -1,23 +1,110 @@
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import { FaFacebook, FaTwitter, FaInstagram ,FaLinkedin ,FaGithub, } from 'react-icons/fa';
 
 function App() {
+  const [japanese, setJapanese] = useState('');
+  const [english, setEnglish] = useState('');
+  const [userInput, setUserInput] = useState('');
+  const [result, setResult] = useState('');
+  const [mistakes, setMistakes] = useState({});
+
+  useEffect(() => {
+    // Fetch the Japanese character and its corresponding English translation from the API
+    fetch('http://localhost:5000/api/next')
+      .then(response => response.json())
+      .then(data => {
+        setJapanese(data.japanese);
+        setEnglish(data.english);
+      })
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleNext = () => {
+    // Fetch the next Japanese character and its corresponding English translation from the API
+    fetch('http://localhost:5000/api/next')
+      .then(response => response.json())
+      .then(data => {
+        setJapanese(data.japanese);
+        setEnglish(data.english);
+        setResult('');
+        setUserInput('');
+      })
+      .catch(error => console.error(error));
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (userInput === english) {
+      setResult('Correct!');
+      // update the correct count
+      setMistakes(prevMistakes => ({
+        ...prevMistakes,
+        [japanese]: {
+          correct: prevMistakes[japanese]?.correct ? prevMistakes[japanese].correct + 1 : 1,
+          incorrect: prevMistakes[japanese]?.incorrect ? prevMistakes[japanese].incorrect : 0
+        }
+      }));
+    } else {
+      setResult('Incorrect!');
+      // update the incorrect count
+      setMistakes(prevMistakes => ({
+        ...prevMistakes,
+        [japanese]: {
+          correct: prevMistakes[japanese]?.correct ? prevMistakes[japanese].correct : 0,
+          incorrect: prevMistakes[japanese]?.incorrect ? prevMistakes[japanese].incorrect + 1 : 1
+        }
+      }));
+    }
+  };
+
+  const charactersWithMistakes = Object.keys(mistakes).filter(char => mistakes[char].correct > 0 || mistakes[char].incorrect > 0);
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="title-container">
+        <h1 className="title">Japanese Practice - 日本</h1>
+        <p className="subtitle">By Raman</p>
+      </div>
+      <div className="main-container">
+        <div className="question-container">
+          <h2 className="question">{`Guess the character?`}</h2>
+          {japanese.includes("/") ? (
+    <>
+      <h1 className="japanese-character">{japanese.split("/")[0]}</h1>
+      <p className="kunyomi">{japanese.split("/")[1]}</p>
+    </>
+  ) : (
+    <h1 className="japanese-character">{japanese}</h1>
+  )}
+        </div>
+        <form onSubmit={handleSubmit}>
+          <input type="text" name="input" onChange={e => setUserInput(e.target.value)} value={userInput} />
+          <p className={`result ${result === 'Correct!' ? 'correct' : 'incorrect'}`}>{result}</p>
+          <div className='button-container'>
+          <button className="next-button" onClick={handleNext} style ={{ marginRight: "20px"}}>Next</button>
+          <button type="submit" className='Submit'>Submit</button>
+        </div>
+        </form>
+        <div className="social-icons">
+          <a href="https://www.linkedin.com/in/ramandeep-maan-b8a105185/"><FaLinkedin /></a>
+          <a href="https://instagram.com/ramandeep11maan?igshid=OTk0YzhjMDVlZA=="><FaInstagram /></a>
+          <a href="https://www.github.com/ramandeep11/"><FaGithub /></a>
+        </div>
+
+      </div>
+      <div className="mistakes-container">
+  {charactersWithMistakes.map(char => (
+    <div key={char} className="mistake">
+    <h2 className="japanese-character">{char}</h2>
+    <div className="mistake-counts">
+      <p className="correct-count">{mistakes[char].correct}</p>
+      <p className="incorrect-count">{mistakes[char].incorrect}</p>
+    </div>
+  </div>
+  ))}
+</div>
     </div>
   );
 }
